d261003b424da1b808dac4831b4cc495d5458524
teacherRay <34903301+teacherRay@users.noreply.github.com>
2019-11-05 17:39:57 +0700

    Top 5 names and HP

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pandas as pd
 df= pd.read_csv('pokemon_data.csv', encoding = "ISO-8859-1")
-print("Heads", df.head(5))
-print("Tails", df.tail(5))
+#xdf= pd.read_excel('pokemon_data.xlsx')
+#tdf= pd.read_txt('pokemon_data.txt' delimited = "/t")
+#print("Heads", df.head(5))
+#print("Tails", xdf.tail(5))
+print (df.columns) #column names
+print(df[['Name','HP']][0:5]) #top five names and HP
