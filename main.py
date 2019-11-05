6cb1fbbf2016f9cd52540a775bbe722329dcc319
teacherRay <34903301+teacherRay@users.noreply.github.com>
2019-11-05 18:42:40 +0700

    3rd  Sorted data

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ df= pd.read_csv('pokemon_data.csv', encoding = "ISO-8859-1")
 #tdf= pd.read_txt('pokemon_data.txt' delimited = "/t")
 #print("Heads", df.head(5))
 #print("Tails", xdf.tail(5))
-print (df.columns) #column names
-print(df[['Name','HP']][0:5]) #top five names and HP
+#print (df.columns) #column names
+#print(df[['Name','HP']][0:5]) #top five names and HP
+#print(df.iloc[1])
+#print(df.loc[df['Type 1'] == 'Grass']) #Search for String values
+#print(df.loc[df['Attack'] > 45]) #Search for Integer values
+#print(df.describe()) #Display mean , min, max, etc stats
+print(df.sort_values('Name', ascending = False)) # Print sorted list
